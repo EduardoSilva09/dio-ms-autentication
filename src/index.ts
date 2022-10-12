994079cd834a4baafe4b26d12b9b96a5d4d3666b
Eduardo Silva Feitosa <88010447+EduardoSilva09@users.noreply.github.com>
2022-10-12 18:04:54 -0300

    Adicionado body parse do express

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import express, { Request, Response, NextFunction } from 'express'
 import userRoute from './routes/users.routes'
 
 const app = express()
+app.use(express.json())
 
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
   res.send({ foo: 'bar' })
 })
-
 app.use(userRoute)
 
 app.listen(3000, () => {
