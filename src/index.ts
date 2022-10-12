ab9c69bd27c41746881f38bdb51b6417044c9b9c
Eduardo Silva Feitosa <88010447+EduardoSilva09@users.noreply.github.com>
2022-10-12 17:14:00 -0300

    Criado servidor http com express

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response, NextFunction } from 'express'
+
+const app = express()
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+  res.send({ foo: 'bar' })
+})
+
+app.listen(3000, () => {
+  console.log('Aplicação executando na porta 3000')
+})
