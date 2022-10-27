b561cc50655d3c13df565d7050f0a60a40dc66e5
Eduardo Silva Feitosa <88010447+EduardoSilva09@users.noreply.github.com>
2022-10-27 20:54:45 -0300

    Tratado erros do banco de dados

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,81 +1,112 @@
 import db from '../db'
+import { DatabaseError } from '../Models/errors/database.error.model'
 import User from '../Models/user.model'
 
 class UserRepository {
   async findAllUsers(): Promise<User[]> {
-    const query = `
-    SELECT 
-        uuid, 
-        username
-    FROM 
-        application_user
-    `
-    const res = await db.query<User>(query)
+    try {
+      const query = `
+      SELECT 
+          uuid, 
+          username
+      FROM 
+          application_user
+      `
+      const res = await db.query<User>(query)
 
-    return res.rows || []
+      return res.rows || []
+    } catch (error) {
+      console.log(error)
+      throw new DatabaseError('Erro na consulta de usuários',error)
+    }
   }
 
   async findById(uuid: string): Promise<User> {
-    const query = `
-    SELECT 
-        uuid, 
-        username
-    FROM 
-        application_user
-    WHERE 
-        uuid = $1
-    `
-    const values = [uuid]
+    try {
+      const query = `
+      SELECT 
+          uuid, 
+          username
+      FROM 
+          application_user
+      WHERE 
+          uuid = $1
+      `
+      const values = [uuid]
 
-    const { rows } = await db.query<User>(query, values)
-    const [user] = rows
+      const { rows } = await db.query<User>(query, values)
+      const [user] = rows
 
-    return user
+      return user
+    } catch (error) {
+      console.log(error)
+      throw new DatabaseError('Erro na consulta de usuário por ID', error)
+    }
   }
 
   async create(user: User): Promise<string> {
-    const query = `
-    INSERT INTO application_user 
-     (
-        username, 
-        password
-     )
-    VALUES ($1, crypt($2, $3))
-    RETURNING uuid
-    `
-    const values = [user.username, user.password, process.env.SALT_KEY]
+    try {
+      const query = `
+      INSERT INTO application_user 
+       (
+          username, 
+          password
+       )
+      VALUES ($1, crypt($2, $3))
+      RETURNING uuid
+      `
+      const values = [user.username, user.password, process.env.SALT_KEY]
 
-    const { rows } = await db.query<{ uuid: string }>(query, values)
-    const [newUser] = rows
+      const { rows } = await db.query<{ uuid: string }>(query, values)
+      const [newUser] = rows
 
-    return newUser.uuid
+      return newUser.uuid
+    } catch (error) {
+      console.log(error)
+      throw new DatabaseError('Erro ao gravar registro do usuário', error)
+    }
   }
-  
-  async update(user: User): Promise<void> {
-    const query = `
-    UPDATE application_user 
-    SET
-        username = $1, 
-        password = crypt($2, $3)
-    WHERE
-        uuid = $4
-    `
-    const values = [user.username, user.password, process.env.SALT_KEY, user.uuid]
 
-    await db.query(query, values)
+  async update(user: User): Promise<void> {
+    try {
+      const query = `
+      UPDATE application_user 
+      SET
+          username = $1, 
+          password = crypt($2, $3)
+      WHERE
+          uuid = $4
+      `
+      const values = [
+        user.username,
+        user.password,
+        process.env.SALT_KEY,
+        user.uuid,
+      ]
+
+      await db.query(query, values)
+    } catch (error) {
+      console.log(error)
+      throw new DatabaseError('Erro ao atualizar registro do usuário', error)
+    }
   }
 
   async remove(uuid: string): Promise<void> {
-    const query = `
-    DELETE 
-    FROM
-       application_user 
-    WHERE
-        uuid = $1
-    `
-    const values = [uuid]
+    try {
+      const query = `
+      DELETE 
+      FROM
+         application_user 
+      WHERE
+          uuid = $1
+      `
+      const values = [uuid]
 
-    await db.query(query, values)
+      await db.query(query, values)
+    } catch (error) {
+      console.log(error)
+      throw new DatabaseError('Erro ao excluir registro do usuário', error)
+    }
   }
 }
 
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response, NextFunction } from 'express'
 import { StatusCodes } from 'http-status-codes'
+import { DatabaseError } from '../Models/errors/database.error.model'
 import userRepository from '../repositories/user.repository'
 
 // get  /users
@@ -13,48 +14,72 @@ const userRoute = Router()
 userRoute.get(
   '/users',
   async (req: Request, res: Response, next: NextFunction) => {
-    const users = await userRepository.findAllUsers()
-    res.send(users)
+    try {
+      const users = await userRepository.findAllUsers()
+      res.send(users)
+    } catch (error) {
+      res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR)
+    }
   },
 )
 
 userRoute.get(
   '/users/:uuid',
   async (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
-    const uuid = req.params.uuid
-    const user = await userRepository.findById(uuid)
-    res.status(StatusCodes.OK).send(user)
+    try {
+      const uuid = req.params.uuid
+      const user = await userRepository.findById(uuid)
+      res.status(StatusCodes.OK).send(user)
+    } catch (error) {
+      if (error instanceof DatabaseError) {
+        res.sendStatus(StatusCodes.BAD_REQUEST)
+      } else {
+        res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR)
+      }
+    }
   },
 )
 
 userRoute.post(
   '/users',
   async (req: Request, res: Response, next: NextFunction) => {
-    const newUser = req.body
-    const uuid = await userRepository.create(newUser)
-    res.status(StatusCodes.CREATED).send(uuid)
+    try {
+      const newUser = req.body
+      const uuid = await userRepository.create(newUser)
+      res.status(StatusCodes.CREATED).send(uuid)
+    } catch (error) {
+      res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR)
+    }
   },
 )
 
 userRoute.put(
   '/users/:uuid',
   async (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
-    const uuid = req.params.uuid
-    const modifiedUser = req.body
-    modifiedUser.uuid = uuid 
+    try {
+      const uuid = req.params.uuid
+      const modifiedUser = req.body
+      modifiedUser.uuid = uuid
 
-    await userRepository.update(modifiedUser)
-    res.sendStatus(StatusCodes.OK)
+      await userRepository.update(modifiedUser)
+      res.sendStatus(StatusCodes.OK)
+    } catch (error) {
+      res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR)
+    }
   },
 )
 
 userRoute.delete(
   '/users/:uuid',
   async (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
-    const uuid = req.params.uuid
+    try {
+      const uuid = req.params.uuid
 
-    await userRepository.remove(uuid)
-    res.sendStatus(StatusCodes.OK)
+      await userRepository.remove(uuid)
+      res.sendStatus(StatusCodes.OK)
+    } catch (error) {
+      res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR)
+    }
   },
 )
 
